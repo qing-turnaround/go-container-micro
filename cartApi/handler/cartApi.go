712170bf74e14452063059ac0933297c5efada42
xing-you-ji <2110833194@qq.com>
2022-03-16 15:53:58 +0800

    complete cartApi

--- cartApi/handler/cartApi.go
+++ cartApi/handler/cartApi.go
@@ -3,48 +3,40 @@ package handler
 import (
 	"context"
 	"encoding/json"
-	log "github.com/micro/go-micro/v2/logger"
+	"errors"
+	"fmt"
+	"strconv"
 
-	"cartApi/client"
-	"github.com/micro/go-micro/v2/errors"
-	api "github.com/micro/go-micro/v2/api/proto"
-	cartApi "path/to/service/proto/cartApi"
+	log "github.com/micro/go-micro/v2/logger"
+	cart "github.com/xing-you-ji/go-container-micro/cart/proto/cart"
+	cartApi "github.com/xing-you-ji/go-container-micro/cartApi/proto/cartApi"
 )
 
-type CartApi struct{}
-
-func extractValue(pair *api.Pair) string {
-	if pair == nil {
-		return ""
-	}
-	if len(pair.Values) == 0 {
-		return ""
-	}
-	return pair.Values[0]
+type CartApi struct {
+	CartService cart.CartService
 }
 
-// CartApi.Call is called by the API as /cartApi/call with post body {"name": "foo"}
-func (e *CartApi) Call(ctx context.Context, req *api.Request, rsp *api.Response) error {
-	log.Info("Received CartApi.Call request")
-
-	// extract the client from the context
-	cartApiClient, ok := client.CartApiFromContext(ctx)
-	if !ok {
-		return errors.InternalServerError("go.micro.api.cartApi.cartApi.call", "cartApi client not found")
+func (e *CartApi) FindAll(ctx context.Context, request *cartApi.Request, response *cartApi.Response) error {
+	log.Info("接收到 /cartApi/findAll 访问请求")
+	if _, ok := request.Get["user_id"]; !ok {
+		return errors.New("参数错误")
 	}
-
-	// make request
-	response, err := cartApiClient.Call(ctx, &cartApi.Request{
-		Name: extractValue(req.Post["name"]),
-	})
+	userIdString := request.Get["user_id"].Value[0]
+	fmt.Println("userID: ", userIdString)
+	userID, err := strconv.ParseInt(userIdString, 10, 64)
 	if err != nil {
-		return errors.InternalServerError("go.micro.api.cartApi.cartApi.call", err.Error())
+		return err
 	}
 
-	b, _ := json.Marshal(response)
+	// 获取购物车的所有商品
+	cartAll, err := e.CartService.GetAll(context.TODO(), &cart.CartFindAll{UserId: userID})
 
-	rsp.StatusCode = 200
-	rsp.Body = string(b)
-
+	// 序列化
+	bytes, err := json.Marshal(cartAll)
+	if err != nil {
+		return err
+	}
+	response.StatusCode = 200
+	response.Body = string(bytes)
 	return nil
 }
--- cartApi/main.go
+++ cartApi/main.go
@@ -1,33 +1,102 @@
 package main
 
 import (
-	log "github.com/micro/go-micro/v2/logger"
+	"context"
+	"fmt"
+	"net"
+	"net/http"
 
+	"github.com/afex/hystrix-go/hystrix"
 	"github.com/micro/go-micro/v2"
-	"cartApi/handler"
-	"cartApi/client"
-
-	cartApi "cartApi/proto/cartApi"
+	"github.com/micro/go-micro/v2/client"
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/registry"
+	consul2 "github.com/micro/go-plugins/registry/consul/v2"
+	roundrobin "github.com/micro/go-plugins/wrapper/select/roundrobin/v2"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	"github.com/opentracing/opentracing-go"
+	cart "github.com/xing-you-ji/go-container-micro/cart/proto/cart"
+	"github.com/xing-you-ji/go-container-micro/cartApi/handler"
+	cartApi "github.com/xing-you-ji/go-container-micro/cartApi/proto/cartApi"
+	"github.com/xing-you-ji/go-container-micro/common"
 )
 
 func main() {
+	// 注册中心
+	consulRegistry := consul2.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.1:8500",
+		}
+	})
+
+	// 链路追踪
+	t, io, err := common.NewTracer("go.micro.api.cartApi", "127.0.0.1:6831")
+	if err != nil {
+		log.Error(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)
+
+	// 熔断器
+	hystrixStreamHandler := hystrix.NewStreamHandler()
+	hystrixStreamHandler.Start()
+
+	go func() {
+		err = http.ListenAndServe(net.JoinHostPort("0.0.0.0", "9096"), hystrixStreamHandler)
+		if err != nil {
+			log.Error(err)
+		}
+	}()
+
 	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.api.cartApi"),
 		micro.Version("latest"),
+		micro.Address("0.0.0.0:8085"),
+		// 添加注册中心
+		micro.Registry(consulRegistry),
+		// 添加链路追踪（客户端）
+		micro.WrapClient(opentracing2.NewClientWrapper(opentracing.GlobalTracer())),
+		// 添加熔断
+		micro.WrapClient(NewClientHystrixWrapper()),
+		// 添加负载均衡
+		micro.WrapClient(roundrobin.NewClientWrapper()),
 	)
 
 	// Initialise service
-	service.Init(
-		// create wrap for the CartApi service client
-		micro.WrapHandler(client.CartApiWrapper(service)),
-	)
+	service.Init()
 
-	// Register Handler
-	cartApi.RegisterCartApiHandler(service.Server(), new(handler.CartApi))
+	cartService := cart.NewCartService("go.micro.service.cart",
+		service.Client())
+
+	// // Register Handler
+	if err := cartApi.RegisterCartApiHandler(service.Server(), &handler.CartApi{CartService: cartService}); err != nil {
+		log.Error(err)
+	}
 
 	// Run service
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type clientWrapper struct {
+	client.Client
+}
+
+func (c *clientWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
+		// run 正常执行裸机
+		fmt.Println(req.Service() + "." + req.Endpoint())
+		return c.Client.Call(ctx, req, rsp, opts...)
+	}, func(err error) error {
+		fmt.Println(err)
+		return err
+	})
+}
+
+func NewClientHystrixWrapper() client.Wrapper {
+	return func(i client.Client) client.Client {
+		return &clientWrapper{i}
 	}
 }
