59ffb86a07d6efbbdf96a1e6da4e33e019a996d0
xing-you-ji <2110833194@qq.com>
2022-03-19 19:16:27 +0800

    alter

--- payment/domain/repository/payment_repository.go
+++ payment/domain/repository/payment_repository.go
@@ -5,18 +5,18 @@ import (
 	"github.com/xing-you-ji/go-container-micro/payment/domain/model"
 )
 
-type IPaymentRepository interface{
-    InitTable() error
-    FindPaymentByID(int64) (*model.Payment, error)
+type IPaymentRepository interface {
+	InitTable() error
+	FindPaymentByID(int64) (*model.Payment, error)
 	CreatePayment(*model.Payment) (int64, error)
 	DeletePaymentByID(int64) error
 	UpdatePayment(*model.Payment) error
-	FindAll()([]model.Payment,error)
-
+	FindAll() ([]model.Payment, error)
 }
+
 // NewPaymentRepository 创建paymentRepository
-func NewPaymentRepository(db *gorm.DB) IPaymentRepository  {
-	return &PaymentRepository{mysqlDb:db}
+func NewPaymentRepository(db *gorm.DB) IPaymentRepository {
+	return &PaymentRepository{mysqlDb: db}
 }
 
 type PaymentRepository struct {
@@ -24,25 +24,25 @@ type PaymentRepository struct {
 }
 
 // InitTable 初始化表
-func (u *PaymentRepository)InitTable() error  {
+func (u *PaymentRepository) InitTable() error {
 	return u.mysqlDb.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").
 		CreateTable(&model.Payment{}).Error
 }
 
 // FindPaymentByID 根据ID查找Payment信息
-func (u *PaymentRepository)FindPaymentByID(paymentID int64) (payment *model.Payment,err error) {
+func (u *PaymentRepository) FindPaymentByID(paymentID int64) (payment *model.Payment, err error) {
 	payment = &model.Payment{}
-	return payment, u.mysqlDb.Model(&model.Payment{}).First(payment,paymentID)
+	return payment, u.mysqlDb.Model(&model.Payment{}).First(payment, paymentID).Error
 }
 
 // CreatePayment 创建Payment信息
-func (u *PaymentRepository) CreatePayment(payment *model.Payment) (paymentID int64,err error) {
+func (u *PaymentRepository) CreatePayment(payment *model.Payment) (paymentID int64, err error) {
 	return payment.ID, u.mysqlDb.Create(payment).Error
 }
 
 // DeletePaymentByID 根据ID删除Payment信息
-func (u *PaymentRepository) DeletePaymentByID(paymentID int64) err error {
-	return u.mysqlDb.Where("ID = ?",paymentID).Delete(&model.Payment{}).Error
+func (u *PaymentRepository) DeletePaymentByID(paymentID int64) error {
+	return u.mysqlDb.Where("ID = ?", paymentID).Delete(&model.Payment{}).Error
 }
 
 // UpdatePayment 更新Payment信息
@@ -51,7 +51,6 @@ func (u *PaymentRepository) UpdatePayment(payment *model.Payment) (err error) {
 }
 
 // FindAll 获取结果集
-func (u *PaymentRepository) FindAll()(paymentAll []model.Payment,err error) {
+func (u *PaymentRepository) FindAll() (paymentAll []model.Payment, err error) {
 	return paymentAll, u.mysqlDb.Find(&paymentAll).Error
 }
-
